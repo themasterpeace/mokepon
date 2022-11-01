6017d5d7192866ca0c80ad01a399ed50c041f347
masterpeace <mesquite275@gmail.com>
2022-10-31 23:52:53 -0600

    Creando funciones de imprimir mensajes de combate y resultado del mismo

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 //Variable Global para ataque judagores
 let ataqueJugador
+let ataqueEnemigo
 
 //Funcion que inicia al momentod e cargar todo el DOM y poder iniciar a jugar 
 function iniciarJuego(){
@@ -62,25 +63,60 @@ function seleccionarMascotaEnemigo(){
    }else if(enemigoAleatorio == 4){
         spanMascotaEnemigo.innerHTML = 'Langostelvis'
    }else if(enemigoAleatorio == 5){
-        spanMascotaEnemigo.innerHTML = 'Tucaplma'
+        spanMascotaEnemigo.innerHTML = 'Tucapalma'
    }else{
         spanMascotaEnemigo.innerHTML = 'Pydos'
    }
 }
-
+//Funciones de ataques 
 function ataqueFuego(){
     ataqueJugador = 'FUEGO'
-    alert(ataqueJugador)
+    ataqueAleatorioEnemigo()
 }
 function ataqueAgua(){
     ataqueJugador = 'AGUA'
-    alert(ataqueJugador)
+    ataqueAleatorioEnemigo()
 }
 function ataqueTierra(){
     ataqueJugador = 'TIERRA'
-    alert(ataqueJugador)
+    ataqueAleatorioEnemigo()
 }
 
+//Funciond e ataques aleatorios del enemigo y/o contrincante
+function ataqueAleatorioEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+
+    if(ataqueAleatorio == 1){
+        ataqueEnemigo = "FUEGO"
+    }else if(ataqueAleatorio == 2){
+        ataqueEnemigo = "AGUA"
+    }else{
+        ataqueEnemigo = "TIERRA"
+    }
+   
+}
+
+ function combate(){
+     if (ataqueEnemigo == ataqueJugador) {
+         crearMensaje("EMPATE")
+     } else if (ataqueJugador == 'FUEGO' && ataqueEnemigo == 'TIERRA') {
+         crearMensaje("GANASTE")
+     } else if (ataqueJugador == 'AGUA' && ataqueEnemigo == 'FUEGO') {
+         crearMensaje("GANASTE")
+     } else if (ataqueJugador == 'TIERRA' && ataqueEnemigo == 'AGUA') {
+         crearMensaje("GANASTE")
+     } else {
+         crearMensaje("PERDISTE")
+     }
+ }
+//Funcion imprimir ataque 
+ function crearMensaje(resultado){
+     let sectionMensajes = document.getElementById('mensajes')
+
+     let parrafo = document.createElement('p')
+     parrafo.innerHTML = 'Tu mascota ataco con' + ataqueJugador + ', la mascota del enemigo ataco con' + ataqueEnemigo + '-' + resultado
+     sectionMensajes.appendChild(parrafo)
+ }
 
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
